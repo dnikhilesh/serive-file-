8050ed92e347b2f58cf6771ad20334a10af2c1b2
dnikhilesh <70903956+dnikhilesh@users.noreply.github.com>
2021-05-30 12:52:04 +0530

    Update .gitscript.sh

--- .gitscript.sh
+++ .gitscript.sh
@@ -1,11 +1,11 @@
 function create() {
     cd
     python3 create.py $1
-    mkdir /Users/ferran/Documents/Projects/$1
-    cd /Users/ferran/Documents/Projects/$1
+    mkdir /Users/dnikhilesh/Documents/Projects/$1
+    cd /Users/ferran/dnikhilesh/Projects/$1
     touch README.md
     git init
-    git remote add origin https://github.com/ferran9908/$1.git
+    git remote add origin https://github.com/dnikhilesh/$1.git
     git add -A
     git commit -m "Initial Commit"
     git push origin master
