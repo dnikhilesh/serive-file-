006a6b12414335e23ab7e7fe77c4830e7d8d5ba0
dnikhilesh <70903956+dnikhilesh@users.noreply.github.com>
2021-05-30 13:03:01 +0530

    Update .gitscript.sh

--- .gitscript.sh
+++ .gitscript.sh
@@ -2,7 +2,7 @@ function create() {
     cd
     python3 create.py $1
     mkdir /Users/dnikhilesh/Documents/Projects/$1
-    cd /Users/ferran/dnikhilesh/Projects/$1
+    cd /Users/dnikhilesh/Documents/Projects/$1
     touch README.md
     git init
     git remote add origin https://github.com/dnikhilesh/$1.git
